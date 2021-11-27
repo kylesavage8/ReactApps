66051aa6737d116749efcf219306a1839d6059f6
kylesavage8 <khadfield8@yahoo.com>
2021-11-27 17:56:49 -0500

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+// src/Header.js
+
+import React from 'react'
+
+function Header() {
+  return (
+    
+    <div>
+       <header>
+ <div className="container">
+     <div id="branding">
+     <h1><span className="highlight">Forensics</span> Tic Tac Toe</h1>
+     </div>	
+     
+</div>
+     </header>
+    </div>
+  
+ )
+
+}
+
+export default Header
---
+++ src/index.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Header from './Header';
+
+/* 
+This branch shows how to fix the 'Warning did you mean className?'
+error in the header component. 
+This error is caused when you copy html into a react component and have not changed the code
+to follow JSX syntax rules. 
+Check out this online utility that you can use to change HTML into JSX:
+https://magic.reactjs.net/htmltojsx.htm
+To use it copy the HTML from the header component in part12 and paset it in to the utility.
+Notice that it will change any instances of 'class' to 'className'.
+*/
+
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+  
+  class Board extends React.Component {
+  
+   renderSquare(i) {
+    return (
+      <Square
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+  
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+  }
+  
+  class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          history: [{
+            squares: Array(9).fill(null),
+          }],
+          stepNumber: 0,
+          xIsNext: true,
+        };
+      }
+
+      handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          history: history.concat([{
+            squares: squares,
+          }]),
+          stepNumber: history.length,
+          xIsNext: !this.state.xIsNext,
+        });
+      }
+      jumpTo(step) {
+        this.setState({
+          stepNumber: step,
+          xIsNext: (step % 2) === 0,
+        });
+      }
+
+      render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+    
+        const moves = history.map((step, move) => {
+          const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+          return (
+            <li key={move}>
+              <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            </li>
+          );
+        });
+    
+        let status;
+        if (winner) {
+          status = 'Winner: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+    
+        return (
+          <div className="game">
+            <div>
+            <Header />
+            </div>
+            <div className="game-board" style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              paddingTop: '60px',
+            
+            }}>
+              <Board
+                squares={current.squares}
+                onClick={(i) => this.handleClick(i)}
+              />
+            </div>
+            <div className="game-info">
+              <div>{status}</div>
+              <ol>{moves}</ol>
+            </div>
+          </div>
+        );
+      }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
+  // This helper function is called from the Board's 'render' function
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
